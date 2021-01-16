cceabaea0023cd4d4195bcb1114e5f2a1fdb0534
Nathan Chow <nathankchow@ucsb.edu>
2021-01-17 07:20:08 +0900

    Add changes.

---
+++ blog.py
@@ -0,0 +1,101 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from yasmp.auth import login_required
+from yasmp.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, event, description, time, location, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        event = request.form['event']
+        description = request.form['description']
+        time = request.form['time']
+        location = request.form['location']
+        error = None
+
+        if not event:
+            error = 'event is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (event, description, time, location, author_id)'
+                ' VALUES (?, ?, ?, ?, ?)',
+                (event, description, time, location, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, event, description, time, location, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        event = request.form['event']
+        description = request.form['description']
+        time = request.form['time']
+        location = request.form['location']
+        
+        error = None
+
+        if not event:
+            error = 'event is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET event = ?, description = ?, time = ?, location = ?'
+                ' WHERE id = ?',
+                (event, description, time, location, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
--- default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.python3Packages.buildPythonApplication {
-  pname = "YASM";
-  src = ./src;
-  version = "0.1";
-  propagatedBuildInputs = [ pkgs.python3Packages.flask ];
-}
