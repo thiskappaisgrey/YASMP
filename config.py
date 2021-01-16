07547ab5cedcf4f960dda922c61b5bcce527cae9
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-01-16 15:23:14 -0800

    Fix minor database config issues

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from os import environ, path
+import os
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
@@ -12,8 +13,10 @@ class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
+
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATABASE="./yasmp.sqlite"
+    # SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # TODO Add GCE/Aws Secrets depending on the ones we want to use
