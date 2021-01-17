c040fc9922b919f72fda8c4a1b846496a2217618
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-01-17 09:45:54 -0800

    Purge unused tailwind styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [ "./app/**/*.html" ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
